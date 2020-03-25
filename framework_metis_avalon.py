7786e80d8c493b912fd2d2f3dc0cf32df0795231
Evan Isenstein <ejisenstein@gmail.com>
2020-03-25 18:29:12 -0400

    adding pdf, early framework

---
+++ framework_metis_avalon.py
@@ -0,0 +1,38 @@
+## Framework for NYC 20 Metis Avalon
+
+## Imports
+
+## Global Variables
+Round = 0 #From 0 to 5.
+Successful Quests = 0: Good guys win at 3, plus Merlin not recognized
+Unsuccessful Quests = = Bad guys win at 3, or Merlin successfully recognized
+
+##Streamlit viz stuff
+
+
+## Players Class
+initialize how many players there are.
+minimum 5
+If 5: 3 Good 2 evil
+If 6: 4 Good 2 evil
+if 7: 4 Good 3 evil
+ask for email addresses, username
+
+## Good vs Evil class:
+Randomly select uses that are good, users that are bad.
+1 of the randomly selected users is Merlin (good) other is Mordor(bad)
+
+## Pick_Leader class:
+Randomly picks first leader from group. The order of the next leaders
+will be visible on streamlit, after 
+
+
+
+## Vote Class
+Sends an email to ALL players with headline Round X: Vote Y:
+
+## Count Vote Class
+
+## Quest Vote class
+
+## Count Quest Vote Class
