b63dabb17514da656f5c816119dd9f5837136870
Evan Isenstein <ejisenstein@gmail.com>
2020-03-30 19:35:40 -0400

    created new db

--- Flask/flask_avalon.py
+++ Flask/flask_avalon.py
@@ -2,11 +2,28 @@ from flask import Flask, redirect, url_for, render_template, request, session, f
 from forms import RegistrationForm, LoginForm
 
 #from datetime import timedelta
-#from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SECRET_KEY'] = '93548cc0fde27fd878cb9aab95033646'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    votes = db.relationship('Vote', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}')"
+
+class Vote(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    vote = db.Column(db.Boolean, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Vote('{self.vote}')"
 
 @app.route("/")
 def home():
