c38f6f3ea55a99eafb8b1d12c0596b8339a2d565
Evan Isenstein <ejisenstein@gmail.com>
2020-03-29 22:19:32 -0400

    new register tab, cleared login

--- Flask/Flask_tutorial.py
+++ Flask/Flask_tutorial.py
@@ -1,13 +1,27 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
+from forms import RegistrationForm, LoginForm
 
 #from datetime import timedelta
 #from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '93548cc0fde27fd878cb9aab95033646'
+
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template("register.html", title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html", title='Login', form=form)
+
 
 
 # @app.route("/login", methods=["POST", "GET"])
--- Flask/forms.py
+++ Flask/forms.py
@@ -1,7 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
-form wtforms.validators import DataRequired, Length
+from wtforms import StringField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length
+
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length()])
+    username = StringField('Username',
+    validators=[DataRequired(), Length(min=2, max=20)])
+
+    submit = SubmitField('Sign Up')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username',
+    validators=[DataRequired(), Length(min=2, max=20)])
+
+    remember = BooleanField('Remember Me')
+
+    submit = SubmitField('Login')
