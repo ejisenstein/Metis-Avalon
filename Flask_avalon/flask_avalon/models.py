f94867b8e4985acd967c824bf70284124a408c66
Evan Isenstein <ejisenstein@gmail.com>
2020-04-02 17:48:58 -0400

    added schema to db

--- Flask_avalon/flask_avalon/models.py
+++ Flask_avalon/flask_avalon/models.py
@@ -4,10 +4,12 @@ from flask_avalon import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable = False)
     votes = db.relationship('Vote', backref='author', lazy=True)
 
     def __repr__(self):
-        return f"User('{self.username}')"
+        return f"User('{self.username}', '{self.email}')"
 
 class Vote(db.Model):
     id = db.Column(db.Integer, primary_key=True)
